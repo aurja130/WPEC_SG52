88af90e42853264045902318898657f3c2298391
Aurora Jahan <amit-hasan.arpon@physics.uu.se>
2026-09-21 23:24:45 +0200

    Scaffolded cpp folder

---
+++ Code/cpp/src/main.cpp
@@ -0,0 +1,26 @@
+// Nuclear Data Adjustment Tool 
+// Copyright (C) 2026 Aurora Jahan
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 as 
+// published by the Free Software Foundation.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# include <iostream>
+# include "glls.hpp"
+# include <highfive/highfive.hpp>
+
+int main() {
+    std::cout << "Nuclear Data Adjustment Tool; Copyright (C) 2026 Aurora Jahan" << std::endl;
+    std::cout << "This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are" << std::endl;
+    std::cout << "welcome to redistribute it under certain conditions. Read LICENSE.txt for details." << std::endl;
+
+    return 0;
+}
